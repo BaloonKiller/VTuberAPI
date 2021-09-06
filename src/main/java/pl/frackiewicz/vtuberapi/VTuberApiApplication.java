d68615a8d048516ae51b8564ad90fb67d5ecbee4
BaloonKiller <afrackiewicz250@gmail.com>
2021-09-06 19:28:35 +0200

    Maven + Spring initial

---
+++ src/main/java/pl/frackiewicz/vtuberapi/VTuberApiApplication.java
@@ -0,0 +1,13 @@
+package pl.frackiewicz.vtuberapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VTuberApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VTuberApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/frackiewicz/vtuberapi/VTuberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.frackiewicz.vtuberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VTuberApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
