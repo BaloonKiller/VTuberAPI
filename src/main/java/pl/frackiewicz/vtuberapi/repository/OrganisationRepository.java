4b8504c497ca0aec82b8e29e0a21958419b5345d
BaloonKiller <afrackiewicz250@gmail.com>
2021-09-08 18:24:21 +0200

    rename

--- src/main/java/pl/frackiewicz/vtuberapi/repository/OrganisationRepository.java
+++ src/main/java/pl/frackiewicz/vtuberapi/repository/OrganisationRepository.java
@@ -1,4 +1,4 @@
-package pl.frackiewicz.vtuberapi.service;
+package pl.frackiewicz.vtuberapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
