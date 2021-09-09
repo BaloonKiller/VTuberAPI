19fb342916a54fc90118bed400a1281f1f64f63e
BaloonKiller <afrackiewicz250@gmail.com>
2021-09-09 23:06:23 +0200

    getAll test

--- src/main/java/pl/frackiewicz/vtuberapi/controller/VTuberController.java
+++ src/main/java/pl/frackiewicz/vtuberapi/controller/VTuberController.java
@@ -1,8 +1,12 @@
 package pl.frackiewicz.vtuberapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.frackiewicz.vtuberapi.entity.VTuber;
 import pl.frackiewicz.vtuberapi.service.VTuberService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vtubers")
@@ -11,5 +15,10 @@ public class VTuberController {
 
     public VTuberController(VTuberService vTuberService) {
         this.vTuberService = vTuberService;
+    }
+
+    @GetMapping("")
+    public List<VTuber> getAllVTubers() {
+        return vTuberService.getAll();
     }
 }
